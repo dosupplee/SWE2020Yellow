4f34a110a67ab2c65f92b176d7f31148e27c8b6c
IsaacLehman <Pizza123#>
2020-03-19 17:01:36 -0400

    CurrentSetup Class created and integrated

--- src/droneSim/CurrentSetup.java
+++ src/droneSim/CurrentSetup.java
@@ -7,69 +7,227 @@ public class CurrentSetup {
 
 	private ArrayList<Meal> allMeals;
 	private ArrayList<Food> allFoods;
-	private ArrayList<Order> orderBacklog;
+	private ArrayList<DeliveryPoint> points;
 	private Map currentMap;
-	
-	public ArrayList<Meal> getAllMeals()
-	{
-		return allMeals;
+
+	public CurrentSetup() {
+		allFoods = new ArrayList<>();
+		allMeals = new ArrayList<>();
+		points = new ArrayList<>();
+		loadFoodSettings();
 	}
-	
-	public Map getCurrentMap()
-	{
+
+	// --------------------------------------------
+	// MAP STUFF
+	// --------------------------------------------
+
+	/**
+	 * @return currentMap
+	 */
+	public Map getCurrentMap() {
 		return currentMap;
 	}
 
-	public void loadFoodSettings() {
-		// TODO load the saved food settings
-	}
-	
-	public void saveFoodSettings() {
-		// TODO save the current food settings
+	/**
+	 * set the current map
+	 * 
+	 * @param map
+	 */
+	public void setCurrentMap(Map map) {
+		currentMap = map;
 	}
 	
 	/**
+	 * Add a delivery point
+	 * @param point
+	 */
+	public void addDeliveryPoint(DeliveryPoint point) {
+		points.add(point);
+	}
+	
+	/**
+	 * remove point by index
+	 * @param pointIndex
+	 */
+	public void removeDeliveryPoint(int pointIndex) {
+		int indexH = points.size() - 1; // last index
+		if (pointIndex < 0 || pointIndex > indexH) { // check if a valid index
+			return; // TODO throw error??
+		}
+		
+		points.remove(pointIndex);
+	}
+	
+	/**
+	 * Remove point by object
+	 * @param point
+	 */
+	public void removeDeliveryPoint(DeliveryPoint point) {
+		if (points.contains(point)) {
+			points.remove(point);
+		}
+	}
+	
+	/**
+	 * returns all the delivery points
+	 * @return points
+	 */
+	public ArrayList<DeliveryPoint> getDeliveryPoints() {
+		return points;
+	}
+
+	
+	// --------------------------------------------
+	// FILE/SETTINGS STUFF
+	// --------------------------------------------
+	
+	public void loadFoodSettings() {
+		// TODO load the saved food settings
+		
+		// Create some basic food stuffs
+		addFood(new Food("Burger", 6));
+		addFood(new Food("Drink", 14));
+		addFood(new Food("FrenchFries", 4));
+
+		// Create some basic meal stuffs ArrayList<Meal>
+		Meal comboMeal = new Meal("Combo Meal", 1.0); // I shouldnt have to give it a weight here
+		comboMeal.addFood(getFood(0));
+		comboMeal.addFood(getFood(1));
+		comboMeal.addFood(getFood(2));
+		addMeal(comboMeal);
+
+		// Create some basic point stuffs
+
+		addDeliveryPoint(new DeliveryPoint(5, 12, "HAL"));
+		addDeliveryPoint(new DeliveryPoint(12, 18, "STEM"));
+		
+		
+		setCurrentMap(new Map("mainMap", "C://MapLocation", getDeliveryPoints()));
+	}
+
+	public void saveFoodSettings() {
+		// TODO save the current food settings
+	}
+
+	// --------------------------------------------
+	// FOOD STUFF
+	// --------------------------------------------
+
+	/**
+	 * @return allFoods
+	 */
+	public ArrayList<Food> getAllFoods() {
+		return allFoods;
+	}
+
+	/**
+	 * @return allMeals
+	 */
+	public ArrayList<Meal> getAllMeals() {
+		return allMeals;
+	}
+
+	/**
 	 * Add meal to meal list
+	 * 
 	 * @param mealToAdd
 	 */
 	public void addMeal(Meal mealToAdd) {
 		allMeals.add(mealToAdd);
+		adjustMealProbabilities(); // readjust the probabilities to = 100%
 	}
-	
+
 	/**
 	 * Add food to food list
+	 * 
 	 * @param foodToAdd
 	 */
 	public void addFood(Food foodToAdd) {
 		allFoods.add(foodToAdd);
 	}
-	
+
+	// TODO throw error??
+	public Food getFood(int foodIndex) {
+		int indexH = allFoods.size() - 1; // last index
+		if (foodIndex < 0 || foodIndex > indexH) { // check if a valid index
+			return new Food("NULL", 0.0); // TODO throw error??
+		}
+
+		return allFoods.get(foodIndex);
+	}
+
+	// TODO throw error??
+	public Meal getMeal(int mealIndex) {
+		int indexH = allMeals.size() - 1; // last index
+		if (mealIndex < 0 || mealIndex > indexH) { // check if a valid index
+			return new Meal("NULL", 0.0); // TODO throw error??
+		}
+
+		return allMeals.get(mealIndex);
+	}
+
 	/**
-	 * removes food from food list and any 
-	 * occurence in a meal
+	 * removes food from food list and any occurence in a meal
 	 * 
 	 * @param foodIndex
 	 */
 	public void deleteFood(int foodIndex) {
 		int indexH = allFoods.size() - 1; // last index
-		if(foodIndex < 0 || foodIndex > indexH) { // check if a valid index
-			return; //TODO throw error??
+		if (foodIndex < 0 || foodIndex > indexH) { // check if a valid index
+			return; // TODO throw error??
 		}
-		
+
 		// if valid, check if food is in a meal
 		for (Meal meal : allMeals) {
 			if (meal.getFoodItems().contains(allFoods.get(foodIndex))) { // if a meal contains food
 				meal.removeFood(allFoods.get(foodIndex)); // remove food from meal
 			}
 		}
-		
+
 		allFoods.remove(foodIndex); // remove from food list
 	}
 	
+	/**
+	 * remove food by object
+	 * removes from allFoods and allMeals
+	 * @param food
+	 */
+	public void deleteFood(Food food) {
+		// check if food is in a meal
+		for (Meal meal : allMeals) {
+			if (meal.getFoodItems().contains(food)) { // if a meal contains food
+				meal.removeFood(food); // remove food from meal
+			}
+		}
+		if(allFoods.contains(food)) {
+			allFoods.remove(food);
+		}
+	}
+
+	/**
+	 * removes meal from meal list
+	 * 
+	 * @param mealIndex
+	 */
 	public void deleteMeal(int mealIndex) {
-		// TODO implement deleting a meal
+		int indexH = allMeals.size() - 1; // last index
+		if (mealIndex < 0 || mealIndex > indexH) { // check if a valid index
+			return; // TODO throw error??
+		}
+
+		allMeals.remove(mealIndex); // remove from meal list
 	}
 	
+	/**
+	 * removes meal by object
+	 * @param meal
+	 */
+	public void deleteMeal(Meal meal) {
+		if (allMeals.contains(meal)) {
+			allMeals.remove(meal); // remove from meal list
+		}
+	}
+
 	/**
 	 * Rescales all the probabilies so they add up to 100
 	 * 
@@ -79,13 +237,13 @@ public class CurrentSetup {
 		if (allMeals.isEmpty()) { // if there are no meals
 			return;
 		}
-		
+
 		// get sum of all probabilities
-		double probSum = 0; 
+		double probSum = 0;
 		for (Meal meal : allMeals) {
 			probSum += meal.getProbability();
 		}
-		
+
 		// set new scaled probabilities (multiply by 100 for % output)
 		for (Meal meal : allMeals) {
 			double newProb = meal.getProbability() / probSum;
--- src/droneSim/Main.java
+++ src/droneSim/Main.java
@@ -5,131 +5,47 @@ package droneSim;
 
 import java.util.ArrayList;
 
-public class Main 
-{
-	
+public class Main {
 	final static int numShifts = 5;
+	private static CurrentSetup currentSetup;
 
-	private static ArrayList<Meal> allMeals;
-	private static ArrayList<Food> allFoods;
-	private static ArrayList<Order> orderBacklog;
-	private static Map currentMap;
-	
 	private static double avgTimeFIFO;
 	private static double slowestTimeFIFO;
 	private static double fastestTimeFIFO;
 	private static double sumFIFO;
-	
+
 	private static double avgTimeKnapsack;
 	private static double slowestTimeKnapsack;
 	private static double fastestTimeKnapsack;
 	private static double sumKnapsack;
-	
-	
-	
-	public static void main(String[] args) 
-	{
-		//Create some basic food stuffs
-		ArrayList<Food> allFoods = new ArrayList<Food>();
-		allFoods.add(new Food("Burger",6));
-		allFoods.add(new Food("Drink",14));
-		allFoods.add(new Food("FrenchFries",4));
-		
-		//Create some basic meal stuffs ArrayList<Meal> 
-		allMeals = new ArrayList<Meal>();
-		Meal comboMeal = new Meal("Combo Meal",1.0); //I shouldnt have to give it a weight here
-		comboMeal.addFood(allFoods.get(0));
-		comboMeal.addFood(allFoods.get(1));
-		comboMeal.addFood(allFoods.get(2));
-		allMeals.add(comboMeal);
-		
-		//Create some basic point stuffs
-		ArrayList<DeliveryPoint> points = new ArrayList<DeliveryPoint>();
-		points.add(new DeliveryPoint(5,12,"HAL"));
-		points.add(new DeliveryPoint(12,18,"STEM"));
-		currentMap = new Map("mainMap","C://MapLocation",points);
-		
-		XMLOrderGenerator gen = new XMLOrderGenerator(numShifts);
+
+	public static void main(String[] args) {
+		// current setup
+		currentSetup = new CurrentSetup();
+
+		XMLOrderGenerator gen = new XMLOrderGenerator(numShifts, currentSetup);
 		gen.generateAllOrders();
-		
+
 	}
-	
-	public static ArrayList<Meal> getAllMeals()
-	{
-		return allMeals;
-	}
-	
-	public static Map getCurrentMap()
-	{
-		return currentMap;
-	}
-	
+
 	public static void createAllOrders() {
 		// TODO generate the orders for the simulation
 	}
-	
+
 	public static void runSimulation() {
 		// TODO run the simulation
 	}
-	
+
 	public static void runTSP() {
-		
+
 	}
-	
+
 	public static void packFIFO() {
 		// TODO pack drones for FIFO simulation
 	}
-	
+
 	public static void packKnapsack() {
 		// TODO pack drones for knapsack simulation
 	}
-	
-	public static void loadFoodSettings() {
-		// TODO load the saved food settings
-	}
-	
-	public static void saveFoodSettings() {
-		// TODO save the current food settings
-	}
-	
-	public static void addMeal(Meal mealToAdd) {
-		// TODO add a new meal
-	}
-	
-	public static void addFood(Food foodToAdd) {
-		// TODO add a new food
-	}
-	
-	public static void deleteFood(int foodIndex) {
-		// TODO implement deleting a food
-	}
-	
-	public static void deleteMeal(int mealIndex) {
-		// TODO implement deleting a meal
-	}
-	
-	
-	/**
-	 * Rescales all the probabilies so they add up to 100
-	 * 
-	 * Run whenever you change the probabilites or add new meals.
-	 */
-	public static void adjustMealProbabilities() {
-		if (allMeals.isEmpty()) { // if there are no meals
-			return;
-		}
-		
-		// get sum of all probabilities
-		double probSum = 0; 
-		for (Meal meal : allMeals) {
-			probSum += meal.getProbability();
-		}
-		
-		// set new scaled probabilities (multiply by 100 for % output)
-		for (Meal meal : allMeals) {
-			double newProb = meal.getProbability() / probSum;
-			meal.setProbability(newProb);
-		}
-	}
-	
+
 }
